4807ef71072ee16d8d84defc9d4f015d0da6899a
Harry Muriithi <hharryjjack@gmail.com>
2023-06-06 05:15:43 -0700

    task 2 trial 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print(chr(i), end="")
+output = ""
+for letter in range(ord('a'), ord('z') + 1):
+    output += chr(letter)
+print("{}".format(output), end="")
