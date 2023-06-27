585d8f7c7375bad43b562b0d00ddd50901823549
Harry Muriithi <hharryjjack@gmail.com>
2023-06-27 10:04:56 -0700

    Write an empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This class represents a square
+    """
+    def __init__(self):
+        """
+        An object square is initialized
+        """
+        pass
