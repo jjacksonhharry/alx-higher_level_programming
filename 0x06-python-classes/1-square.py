5188d9334da16c382fc78ab7e72e0f0738937270
Harry Muriithi <hharryjjack@gmail.com>
2023-06-27 11:44:32 -0700

    Write a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Class that represents a square
+    """
+    def __init__(self, size=0):
+        """
+        Initialize a square object with an optional size.
+
+        Args:
+            size int: size of square defalult set to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
