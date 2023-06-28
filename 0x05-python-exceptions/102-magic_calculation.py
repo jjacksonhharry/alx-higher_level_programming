b8bb81ca910b2f394004d31e30b24092fcfa9f30
Harry Muriithi <hharryjjack@gmail.com>
2023-06-28 13:10:16 -0700

    Write the Python function def magic_calculation(a, b)

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -10,6 +10,6 @@ def magic_calculation(a, b):
             else:
                 result += (a ** b) / i
         except Exception:
-            result += b + a
+            result = b + a
             break
         return result
