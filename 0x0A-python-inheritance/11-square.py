9646976c68d74e1a70c0b1307b21eddd8f26bcbf
Harry Muriithi <hharryjjack@gmail.com>
2023-07-12 10:35:54 -0700

    class BaseGeometry (based on 6-base_geometry.py)

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -24,4 +24,7 @@ class Square(Rectangle):
         Returns:
             str: The string representation of the instance
         """
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+        return "[Square] {}/{}".format(
+                self._Rectangle__width,
+                self._Rectangle__height
+                )
