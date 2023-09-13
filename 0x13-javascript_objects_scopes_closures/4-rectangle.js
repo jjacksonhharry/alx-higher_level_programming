b48fdc0b3d264178a3a76c69ec75f250daf5232e
Harry Muriithi <hharryjjack@gmail.com>
2023-09-13 10:51:37 +0300

    class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -15,13 +15,13 @@ class Rectangle {
    }
 
   rotate () {
-     const temp = this.width;
-     this.width = this.height;
-     this.height = temp;
-   }
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
   double () {
-     this.height = this.height * 2;
-     this.width = this.width * 2;
-   }
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
 };
 module.exports= Rectangle;
