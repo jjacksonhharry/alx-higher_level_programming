77b1ee28308a7aac67c86e3f8ba85ac55589e447
Harry Muriithi <hharryjjack@gmail.com>
2023-09-17 16:04:18 +0300

    fixing error task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,12 +13,17 @@ if __name__ == "__main__":
     Access the database
     """
     if len(sys.argv) != 4:
-        print("Usage: {} <DB_USER> <DB_PASSWORD> <DB_NAME>".format(sys.argv[0]))
+        print(
+                "Usage: {} <DB_USER> <DB_PASSWORD> <DB_NAME>"
+                .format(sys.argv[0]))
         sys.exit(1)
 
     db_user, db_password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(db_user, db_password, db_name))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        db_user,
+        db_password,
+        db_name))
 
     Session = sessionmaker(bind=engine)
     session = Session()
