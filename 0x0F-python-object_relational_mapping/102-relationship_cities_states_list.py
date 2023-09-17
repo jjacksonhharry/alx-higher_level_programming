7928fe2bf40f5e486b9caeefb0ca1117b6379d4b
Harry Muriithi <hharryjjack@gmail.com>
2023-09-17 15:21:57 +0300

    script that lists all City objects from the database hbtn_0e_101_usa

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script that deletes all State objects with
+a name containing the letter a from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == "__main__":
+    """
+    Access the database
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    
+    stat = session.query(State).join(City).order_by(City.id).all()
+
+    for state in stat:
+        for city in state.cities:
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
