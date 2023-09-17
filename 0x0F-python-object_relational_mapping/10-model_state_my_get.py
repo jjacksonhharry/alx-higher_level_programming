db16a611e91a8041384277dae33452cefb3a3ba7
Harry Muriithi <hharryjjack@gmail.com>
2023-09-17 14:33:04 +0300

    script that prints the first State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script that prints the State object with the
+name passed as argument from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """
+    Access the database
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+             argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state is not None:
+        print('{0}: {1}'.format(state.id))
+    else:
+        print("Not found")
