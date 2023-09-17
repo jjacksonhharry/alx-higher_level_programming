f5e9edecb653918254f560ff22e5bd35451e72f9
Harry Muriithi <hharryjjack@gmail.com>
2023-09-17 15:30:04 +0300

    script that lists all City objects from the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -4,7 +4,7 @@ script that deletes all State objects with
 a name containing the letter a from the database hbtn_0e_6_usa
 """
 
-from sys import argv
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     Access the database
     """
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
+    db_url = ("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     engine = create_engine(db_url)
     Base.metadata.create_all(engine)
