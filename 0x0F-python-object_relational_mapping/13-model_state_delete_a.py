585fdaea21192ff896672f5bac6358f0430654e1
Harry Muriithi <hharryjjack@gmail.com>
2023-09-17 14:44:48 +0300

    script that prints the first State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script that deletes all State objects with
+a name containing the letter a from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """
+    Access the database
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+             argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states_to_delete:
+        session.delete(state)
+
+    session.commit()
+
+    session.close()
