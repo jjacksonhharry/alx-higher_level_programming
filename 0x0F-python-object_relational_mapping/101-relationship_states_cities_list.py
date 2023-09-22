6bc5bfeb611c5acddd56bf5679e1b8cdb3b5bf63
Harry Muriithi <hharryjjack@gmail.com>
2023-09-22 15:08:22 +0300

    Write a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -25,9 +25,12 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+    states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+        cities = state.cities
+        for city in cities:
+            print("\t{}: {}".format(city.id, city.name))
+
+    session.close()
