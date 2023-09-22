351c7195a7d0df9166030a6fb1428fba3e2d9db6
Harry Muriithi <hharryjjack@gmail.com>
2023-09-22 14:18:35 +0300

    Write a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     session = Session()
 
-    states = session.query(state).filter(State.name.contains('a'))
+    states = session.query(State).filter(State.name.contains('a'))
     if state is not None:
         for state in states:
             print('{0}: {1}'.format(state.id, state.name))
