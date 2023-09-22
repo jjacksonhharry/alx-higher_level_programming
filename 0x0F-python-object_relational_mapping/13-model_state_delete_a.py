bb841e1e5440026eba5b7914144ec87850e9b029
Harry Muriithi <hharryjjack@gmail.com>
2023-09-22 14:51:00 +0300

    Write a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -22,7 +22,9 @@ if __name__ == "__main__":
 
     session = Session()
 
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    states_to_delete = """
+                session.query(State).filter(State.name.like('%a%')).all()
+                """
     for state in states_to_delete:
         session.delete(state)
 
