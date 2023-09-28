f7bba05b9a3a0d3633f4234be2bb8f8878e325f2
Harry Muriithi <hharryjjack@gmail.com>
2023-09-28 20:27:11 +0300

    script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+url="$1"
+json_file="$2"
+
+curl -s -H "Content-Type: application/json" -d @"$json_file" "$url"
