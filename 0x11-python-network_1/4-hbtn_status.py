56311423fdef66548139406b7a46bb1cca468186
Harry Muriithi <hharryjjack@gmail.com>
2023-10-02 19:39:27 +0300

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,12 +3,10 @@
 Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
-url = 'https://alx-intranet.hbtn.io/status'
 
 
 if __name__ == "__main__":
-    response = requests.get(url)
-    response.raise_for_status()
+    r = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
